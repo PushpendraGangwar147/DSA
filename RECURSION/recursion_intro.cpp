c42dce01b9240481093b79e18b32a0bc5615372a
PushpendraGangwar147 <gangwarvikash80@gmail.com>
2025-10-14 23:55:08 +0530

    Add files via upload

---
+++ RECURSION/recursion_intro.cpp
@@ -0,0 +1,25 @@
+// RECURSION Introdunction :-
+
+#include<iostream>
+using namespace std;
+
+// Recursive Function 
+void fun(int n){
+    if(n == 0){   // base condition
+        cout<<"Happy Birthday ";
+        return;
+    }
+    cout<< n <<"days left for birthday" <<endl;
+    fun(n-1);
+}
+
+int main(){
+    int n;
+    cout<<"How many days left for birthday : ";
+    cin >> n;
+    fun(n);
+
+
+
+    return 0;
+}
