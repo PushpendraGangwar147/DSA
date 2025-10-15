d31e99606466e098fedada4598a44043a647d2e7
PushpendraGangwar147 <gangwarvikash80@gmail.com>
2025-10-15 19:40:52 +0530

    Add files via upload

---
+++ RECURSION/power_of_two.cpp
@@ -0,0 +1,22 @@
+// Power of 2 using recursion :-
+
+#include<iostream>
+using namespace std;
+
+int power(int num, int n){
+    if(n == 1) return 2;
+
+    return (2* power(2, n-1));
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number : ";
+    cin >> n;
+
+    cout<<"The Power of 2 is : "<< power(2, n);
+
+
+    return 0;
+}
